e931d11efa8617900ecb37212f2f0b1fe87c792d
bgalvao90 <faculdadebgalvas@gmail.com>
2025-04-20 23:23:36 -0300

    atividade04

---
+++ Atividade04/Program.cs
@@ -0,0 +1,72 @@
+﻿using Atividade04;
+
+Console.Write("Digite o numero da vaga: ");
+int numeroVaga = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o tipo do veiculo (carro, moto ou caminhão): ");
+string tipoVeiculo = Console.ReadLine().ToLower();
+VagaEstacionamento vaga = new VagaEstacionamento(numeroVaga, tipoVeiculo);
+
+vaga.OcuparVaga();
+
+Console.WriteLine();
+Console.Write("Deseja alterar tipo do veiculo (s para sim) ou (n para nao) : ");
+char opcao = char.Parse(Console.ReadLine().ToLower());
+if (opcao == 's')
+{
+    if (!vaga.Ocupada)
+    {
+        Console.Write("Digite o novo tipo do veiculo (carro, moto ou caminhão): ");
+        string novoTipo = Console.ReadLine().ToLower();
+        vaga.AlterarTipoVeiculo(novoTipo);
+        vaga.ExibirInformacoes();
+    }
+
+    else
+    {
+        
+        Console.WriteLine("Tipo de veículo não alterado.");
+        Console.Write("Deseja liberar a vaga para alterar o veiculo? ");
+        opcao = char.Parse(Console.ReadLine().ToLower());
+
+        if (opcao == 's') { 
+        vaga.LiberarVaga();
+        vaga.ExibirInformacoes();
+
+        Console.Write("Digite o novo tipo do veiculo (carro, moto ou caminhão): ");
+        string novoTipo = Console.ReadLine().ToLower();
+        vaga.AlterarTipoVeiculo(novoTipo);
+        Console.WriteLine();
+        
+        Console.WriteLine();
+        vaga.ExibirInformacoes();
+
+            Console.Write("Deseja ocupar a vaga novamente? ");
+            opcao = char.Parse(Console.ReadLine().ToLower());
+            if (opcao == 's')
+            {
+                vaga.OcuparVaga();
+                Console.WriteLine();
+                vaga.ExibirInformacoes();
+            }
+            else
+            {
+                Console.WriteLine("Vaga livre.");
+                vaga.ExibirInformacoes();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Vaga não liberada.");
+            vaga.ExibirInformacoes();
+        }
+
+    }
+    
+}
+else
+{
+    Console.WriteLine("Tipo de veículo não alterado.");
+    vaga.ExibirInformacoes();
+}
+
---
+++ Atividade04/VagaEstacionamento.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade04
+{
+    class VagaEstacionamento
+    {
+     
+        public int NumeroVaga { get; private set; }
+        public bool Ocupada { get; private set; }
+        public string TipoVeiculo { get; private set; }
+
+        public VagaEstacionamento(int numerovaga, string tipoveiculo)
+        {
+            if (!TipoValido(tipoveiculo))
+            {
+                throw new ArgumentException("Tipo de veículo inválido. Aceito: carro, moto ou caminhão.");
+            }
+
+            NumeroVaga = numerovaga;
+            TipoVeiculo = tipoveiculo;
+            Ocupada = false;
+        }
+
+        public void OcuparVaga()
+        {
+            Console.WriteLine("Vaga Ocupada");
+            Ocupada = true;
+        }
+
+        public void LiberarVaga()
+        {
+            Console.WriteLine("Vaga liberada!");
+            Ocupada = false;
+        }
+
+        public void AlterarTipoVeiculo(string novoTipo)
+        {
+            if (!Ocupada)
+            {
+                Console.WriteLine("Tipo de veículo alterado com sucesso.");
+                TipoVeiculo = novoTipo;
+            }
+
+            if (TipoValido(novoTipo))
+            {
+                TipoVeiculo = novoTipo;
+            }
+
+            else
+            {
+                Console.WriteLine("A vaga está ocupada. Não é possível alterar o tipo de veículo.");
+            }
+        }
+
+        public void ExibirInformacoes()
+        {
+
+            Console.WriteLine($"Número da Vaga: {NumeroVaga}");
+            Console.WriteLine($"Tipo de Veículo: {TipoVeiculo}");
+            Console.WriteLine($"{(Ocupada ? "Ocupada" : "Livre")}");
+        }
+
+        public bool TipoValido(string tipoVeiculo)
+        {
+            return tipoVeiculo == "carro" || tipoVeiculo == "moto" || tipoVeiculo == "caminhão" || tipoVeiculo == "caminhao";
+        }
+    }
+    
+}
